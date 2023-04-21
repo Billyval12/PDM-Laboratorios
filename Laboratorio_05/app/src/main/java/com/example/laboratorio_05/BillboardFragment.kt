9556e3abf25f2e69b400a058f7bddd247e87c43f
Billyval12 <00124621@uca.edu.sv>
2023-04-21 15:33:43 -0600

    Add: Laboratorio 05

---
+++ Laboratorio_05/app/src/main/java/com/example/laboratorio_05/BillboardFragment.kt
@@ -0,0 +1,43 @@
+package com.example.laboratorio_05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class BillboardFragment : Fragment() {
+
+    private lateinit var movieCardView: CardView
+    private lateinit var plusButton: FloatingActionButton
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_billboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        movieCardView = view.findViewById(R.id.movie_card_view)
+        plusButton = view.findViewById(R.id.floatingActionButton)
+
+        movieCardView.setOnClickListener {
+            it.findNavController().navigate(R.id.action_billboardFragment_to_movieFragment)
+        }
+
+        plusButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_billboardFragment_to_createMovieFragment)
+        }
+
+    }
+
+}
